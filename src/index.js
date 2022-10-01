3a8ecd518ffe06756d93bf9ed8db8ff5ae7df0ca
AegisVP <vlad@pysarenko.com>
2022-10-01 18:38:38 +0200

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import theme from 'components/Common/Theme.jsx';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from 'styled-components';
+import { App } from 'components/App/App';
+import { theme } from './components/Common/Theme';
+import { persistor, store } from './redux/store';
 import './index.css';
 
-///////////////////
-// Маршрутизация //
-///////////////////
-//
-// Если приложение использует библиотеку react-router-dom для маршрутизации,
-// необходимо дополнительно настроить компонент < BrowserRouter >,
-// передав в пропе basename точное название твоего репозитория.
-// Слеши в начале и конце строки обязательны.
-//
-// <BrowserRouter basename="/_____repo_name______/">
-//   <App />
-// </BrowserRouter>
-//
-///////////////////
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <PersistGate loading="loading..." persistor={persistor}>
+          <App />
+        </PersistGate>
+      </React.StrictMode>
+    </Provider>
+  </ThemeProvider>
 );
